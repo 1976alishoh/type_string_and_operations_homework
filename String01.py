396a1d3f88d9ee84a4e4aac98ff04ef631220c84
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-10-28 16:47:54 +0300

    01 done

--- String01.py
+++ String01.py
@@ -1,8 +1,10 @@
 def main():
+    
     """"Return an optional string of length eight
     Args:
         None
     Returns:
         str: return answer.
     """
-    return 
+    return str(12345678)
+print(main())
