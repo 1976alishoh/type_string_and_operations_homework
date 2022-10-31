9e9d64bf7b755eefa2be5187bf673dc94dbb57cd
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-10-31 12:08:06 +0300

    01 done

--- String01.py
+++ String01.py
@@ -1,12 +1,12 @@
 def main():
-    a = "\"12345678\""
+    
     """"Return an optional string of length eight
     Args:
         None
     Returns:
         str: return answer.
     """
-    return a
+    return "12345678"
 print(main())
 
 
